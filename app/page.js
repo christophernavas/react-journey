5a4586eb0f9284614f72294e0c33e521a5297153
Christopher <christo.navas@hotmail.fr>
2024-12-16 21:01:02 +0100

    fixing Suspense error

--- app/page.js
+++ app/page.js
@@ -4,9 +4,10 @@ import { Header } from "./components/Header/Header";
 import { LateralMenu } from "./components/LateralMenu/LateralMenu";
 import { Card } from "./components/Card/Card";
 import { REACT_CARDS } from "./react-cards";
+import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
-export default function Home() {
+function FilteredCards() {
   const currentFilter = useSearchParams().get("filter");
   const filteredMap =
     currentFilter === "all" || !currentFilter
@@ -14,21 +15,29 @@ export default function Home() {
       : REACT_CARDS.filter((card) => card.category === currentFilter);
 
   return (
-    <div className="container mx-auto lg:max-w-screen-lg">
-      <Header />
-      <div className="flex gap-4">
-        <LateralMenu />
-        <div className="grid grid-cols-3 gap-5 pb-10 w-full h-full overflow-auto">
-          {filteredMap.map((card) => (
-            <Card
-              key={card.name}
-              title={card.name}
-              categorie={card.category}
-              url={card.url}
-            />
-          ))}
+    <div className="grid grid-cols-3 gap-5 pb-10 w-full h-full overflow-auto">
+      {filteredMap.map((card) => (
+        <Card
+          key={card.name}
+          title={card.name}
+          categorie={card.category}
+          url={card.url}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default function Home() {
+  return (
+    <Suspense>
+      <div className="container mx-auto lg:max-w-screen-lg">
+        <Header />
+        <div className="flex gap-4">
+          <LateralMenu />
+          <FilteredCards />
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
